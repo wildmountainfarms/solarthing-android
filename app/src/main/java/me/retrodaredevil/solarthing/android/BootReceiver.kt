52084772cd14c393cd62c1d67aceab745cc31749
Joshua Shannon <retrodaredevil@gmail.com>
2019-12-07 21:50:56 -0600

    Fixed boot bug. Version 1.21 mark

--- app/src/main/java/me/retrodaredevil/solarthing/android/BootReceiver.kt
+++ app/src/main/java/me/retrodaredevil/solarthing/android/BootReceiver.kt
@@ -4,25 +4,36 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.widget.Toast
 import me.retrodaredevil.solarthing.android.service.startServiceIfNotRunning
 
 class BootReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        if(context != null && intent != null) {
-            if(intent.action!!.endsWith(".BOOT_COMPLETED")){
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-                    println("Got boot completed. We should have got lock boot completed earlier.")
+    private var alreadyStarted = false
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action!!
+//        Toast.makeText(context, "Received action: $action hashCode: ${hashCode()}", Toast.LENGTH_LONG).show()
+        if(action.endsWith(".BOOT_COMPLETED")){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                println("Got boot completed. We should have got lock boot completed earlier.")
+                if(alreadyStarted){
+                    println("Good")
+                    Toast.makeText(context, "Checking to make sure SolarThing is started.", Toast.LENGTH_LONG).show()
+                    startServiceIfNotRunning(context)
                     return
+                } else {
+                    println("Even though SDK_INT >= N, this isn't started yet!")
+                    Toast.makeText(context, "SolarThing: Report this: Unexpected boot alreadyStarted=false", Toast.LENGTH_LONG).show()
                 }
             }
-            println("Received on boot!")
-            val miscProfile = createMiscProfileProvider(context).activeProfile
-            if(miscProfile.startOnBoot) {
-                println("Starting on boot")
-                startServiceIfNotRunning(context)
-            } else {
-                println("Start on boot not enabled!")
-            }
+        }
+        println("Received on boot!")
+        val miscProfile = createMiscProfileProvider(context).activeProfile
+        if(miscProfile.startOnBoot) {
+            println("Starting on boot")
+            startServiceIfNotRunning(context)
+            alreadyStarted = true
+        } else {
+            println("Start on boot not enabled!")
         }
     }
 
