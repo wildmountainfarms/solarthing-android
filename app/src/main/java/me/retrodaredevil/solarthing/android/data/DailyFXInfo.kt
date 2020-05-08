5401290f51ec2e752f4209d031fedd8ce9b1f5c2
Joshua Shannon <retrodaredevil@gmail.com>
2020-05-07 19:30:54 -0500

    Started on implementing SolarDailyInfo

--- app/src/main/java/me/retrodaredevil/solarthing/android/data/DailyFXInfo.kt
+++ app/src/main/java/me/retrodaredevil/solarthing/android/data/DailyFXInfo.kt
@@ -1,17 +1,28 @@
 package me.retrodaredevil.solarthing.android.data
 
+import me.retrodaredevil.solarthing.solar.outback.fx.extra.DailyFXPacket
+
 class DailyFXInfo(
     val inverterKWH: Float,
     val buyKWH: Float,
     val chargerKWH: Float,
     val sellKWH: Float
 ) {
+    constructor(dailyFXPacket: DailyFXPacket) : this(dailyFXPacket.inverterKWH, dailyFXPacket.buyKWH, dailyFXPacket.chargerKWH, dailyFXPacket.sellKWH)
     operator fun plus(dailyFXInfo: DailyFXInfo): DailyFXInfo {
         return DailyFXInfo(
             inverterKWH + dailyFXInfo.inverterKWH,
             buyKWH + dailyFXInfo.buyKWH,
             chargerKWH + dailyFXInfo.chargerKWH,
             sellKWH + dailyFXInfo.sellKWH
+        )
+    }
+    operator fun minus(dailyFXInfo: DailyFXInfo): DailyFXInfo {
+        return DailyFXInfo(
+            inverterKWH - dailyFXInfo.inverterKWH,
+            buyKWH - dailyFXInfo.buyKWH,
+            chargerKWH - dailyFXInfo.chargerKWH,
+            sellKWH - dailyFXInfo.sellKWH
         )
     }
 }
--- app/src/main/java/me/retrodaredevil/solarthing/android/data/SolarDailyInfo.kt
+++ app/src/main/java/me/retrodaredevil/solarthing/android/data/SolarDailyInfo.kt
@@ -1,10 +1,28 @@
 package me.retrodaredevil.solarthing.android.data
 
-import me.retrodaredevil.solarthing.packets.collection.PacketGroup
+import me.retrodaredevil.solarthing.packets.Packet
+import me.retrodaredevil.solarthing.packets.collection.FragmentedPacketGroup
 import me.retrodaredevil.solarthing.packets.identification.IdentifierFragment
+import me.retrodaredevil.solarthing.solar.common.DailyChargeController
+import me.retrodaredevil.solarthing.solar.common.DailyData
+import me.retrodaredevil.solarthing.solar.daily.DailyConfig
+import me.retrodaredevil.solarthing.solar.daily.DailyPair
+import me.retrodaredevil.solarthing.solar.daily.DailyUtil
+import me.retrodaredevil.solarthing.solar.outback.fx.extra.DailyFXPacket
+import me.retrodaredevil.solarthing.solar.outback.mx.MXStatusPacket
+import me.retrodaredevil.solarthing.solar.renogy.rover.RoverStatusPacket
 
-fun createSolarDailyInfo(packetGroups: List<PacketGroup>): SolarDailyInfo {
-    TODO("Still have to implement")
+
+fun createSolarDailyInfo(dayStartTimeMillis: Long, packetGroups: List<FragmentedPacketGroup>): SolarDailyInfo {
+    val dailyConfig = DailyConfig(dayStartTimeMillis + 3 * 60 * 60 * 1000, dayStartTimeMillis + 10 * 60 * 60 * 1000)
+
+    val mxMap = DailyUtil.getDailyPairs(DailyUtil.mapPackets(MXStatusPacket::class.java, packetGroups), dailyConfig)
+    val roverMap = DailyUtil.getDailyPairs(DailyUtil.mapPackets(RoverStatusPacket::class.java, packetGroups), dailyConfig)
+    val dailyFXMap = DailyUtil.getDailyPairs(DailyUtil.mapPackets(DailyFXPacket::class.java, packetGroups), dailyConfig)
+    return SolarDailyInfo(
+        mxMap.mapValues { getTotal(it.value, DailyChargeController::getDailyKWH) } + roverMap.mapValues { getTotal(it.value, DailyChargeController::getDailyKWH) },
+        dailyFXMap.mapValues { getDailyFXTotal(it.value, ::DailyFXInfo) }
+    )
 }
 
 class SolarDailyInfo(
@@ -12,4 +30,33 @@ class SolarDailyInfo(
     val fxMap: Map<IdentifierFragment, DailyFXInfo>
 ) {
     val dailyFXInfo: DailyFXInfo? = if (fxMap.isEmpty()) null else fxMap.values.reduce { sum, element -> sum + element }
+}
+
+fun <T> getTotal(
+    dailyPairs: List<DailyPair<T>>,
+    totalGetter: (T) -> Float
+): Float where T : Packet, T : DailyData {
+    var total = 0f
+    for (dailyPair in dailyPairs) {
+        total += if (dailyPair.startPacketType == DailyPair.StartPacketType.CUT_OFF) {
+            totalGetter(dailyPair.latestPacket.packet) - totalGetter(dailyPair.startPacket.packet)
+        } else {
+            totalGetter(dailyPair.latestPacket.packet)
+        }
+    }
+    return total
+}
+fun <T> getDailyFXTotal(
+    dailyPairs: List<DailyPair<T>>,
+    totalGetter: (T) -> DailyFXInfo
+): DailyFXInfo where T : Packet, T : DailyData {
+    var total = DailyFXInfo(0.0f, 0.0f, 0.0f, 0.0f)
+    for (dailyPair in dailyPairs) {
+        total += if (dailyPair.startPacketType == DailyPair.StartPacketType.CUT_OFF) {
+            totalGetter(dailyPair.latestPacket.packet) - totalGetter(dailyPair.startPacket.packet)
+        } else {
+            totalGetter(dailyPair.latestPacket.packet)
+        }
+    }
+    return total
 }
